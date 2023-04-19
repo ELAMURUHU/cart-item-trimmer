7a8029fcfef23885302db6167593d4f6e2c2ca9d
ELAMURUHU <elamuruhu.manoharan@gmail.com>
2023-04-19 09:52:42 +0530

    update

---
+++ js/main.js
@@ -0,0 +1,503 @@
+let trimmer=[
+    {
+       
+            id:192343,
+            brand:"syska",
+            color:"black",
+            size:"0.5mm to 20mm",
+            bladematerial:"stainless-steel",
+            batteryruntime:"60 mins",
+            price:100,
+			pic:"images/syska-4.jpg",
+			
+    }, 
+    
+    
+    {
+            id:223543,
+		    brand:"boat",
+            color:"blue",
+            size:"0.5mm to 20mm",
+            bladematerial:"steel",
+            batteryruntime:"50 mins",
+            price:400,
+			pic:"images/trimmer-1.jpg",
+			
+    },
+    
+    
+       { 
+             id:323456,
+		     brand:"philips",
+            color:"green",
+            size:"0.5mm to 20mm",
+            bladematerial:"wood",
+            batteryruntime:"90 mins",
+            price:700,
+			pic:"images/trimmer-2.jpg",
+			
+        },
+    
+    
+        {   
+		    id:567890,
+            brand:"beardo",
+            color:"cyan",
+            size:"0.5mm to 20mm",
+            bladematerial:"brass",
+            batteryruntime:"55 mins",
+            price:600,
+			pic:"images/trimmer-3.jpg",
+			
+        },
+    
+    
+      { 
+        
+		    id:567823, 
+		    brand:"wahl",
+            color:"yellow",
+            size:"0.5mm to 20mm",
+            bladematerial:"gold",
+            batteryruntime:"150 mins",
+            price:2000,
+			pic:"images/trimmer-4.jpg",
+			
+      },
+    
+    
+    {      
+	
+	        id:567822,
+	        brand:"panasonic",
+            color:"saffron",
+            size:"0.5mm to 20mm",
+            bladematerial:"steel",
+            batteryruntime:"50 mins",
+            price:900,
+			pic:"images/trimmer-5.jpg",
+			
+   },
+    
+	 {
+            id:123456,
+            brand:"andis",
+            color:"purple",
+            size:"0.5mm to 30mm",
+            bladematerial:"steel-wood",
+            batteryruntime:"90 mins",
+            price:2000,
+			pic:"images/trimmer-6.jpg",
+		
+    } ,
+    
+	{
+            id:098765,
+            brand:"halteker",
+            color:"black",
+            size:"0.5mm to 20mm",
+            bladematerial:"plastic",
+            batteryruntime:"100 mins",
+            price:1000,
+			pic:"images/trimmer-7.jpg",
+			
+       
+	},
+    
+    {
+	        id:098766,
+            brand:"zlade",
+            color:"navy-blue",
+            size:"0.5mm to 20mm",
+            bladematerial:"copper",
+            batteryruntime:"50 mins",
+            price:499,
+			pic:"images/trimmer-8.jpg",
+			
+       
+	},
+    
+	{
+            id:098777,
+            brand:"braun",
+            color:"blue",
+            size:"0.5mm to 20mm",
+            bladematerial:"steel-zinc",
+            batteryruntime:"50 mins",
+            price:1299,
+			pic:"images/trimmer-9.jpg",
+		
+       
+	},
+    
+	{
+            id:098888,
+            brand:"mi",
+            color:"gold",
+            size:"0.5mm to 22mm",
+            bladematerial:"erythroxylem",
+            batteryruntime:"50 mins",
+            price:2500,
+			pic:"images/trimmer-10.jpg",
+			
+       
+	},
+    
+	{
+            id:098432,
+            brand:"VGR",
+            color:"blue",
+            size:"0.5mm to 20mm",
+            bladematerial:"brown",
+            batteryruntime:"45 mins",
+            price:1500,
+			pic:"images/trimmer-11.jpg",
+			
+       
+	},
+    
+	{
+            id:098433,
+            brand:"RECONNECT",
+            color:"white",
+            size:"0.5mm to 10mm",
+            bladematerial:"gray-steel",
+            batteryruntime:"90 mins",
+            price:4000,
+			pic:"images/trimmer-12.jpg",
+       
+	},
+    
+	{
+            id: 0983431,
+            brand:"letsshare",
+            color:"dark-blue",
+            size:"0.5mm to 20mm",
+            bladematerial:"diamond",
+            batteryruntime:"50 mins",
+            price:5000,
+			pic:"images/trimmer-13.jpg",
+       
+	},
+    
+	{
+            id:0983450,
+            brand:"vega",
+            color:"ligth-blue",
+            size:"0.5mm to 20mm",
+            bladematerial:"steel",
+            batteryruntime:"50 mins",
+            price:500,
+			pic:"images/trimmer-14.jpg",
+       
+	},
+    
+	
+	{
+            id:111111,
+            brand:"beaverstrong",
+            color:"light-black",
+            size:"0.5mm to 20mm",
+            bladematerial:"rubber",
+            batteryruntime:"50 mins",
+            price:800,
+			pic:"images/trimmer-15.jpg",
+       
+	},
+    
+	{
+            id:222280,
+            brand:"htc",
+            color:"red-blue",
+            size:"0.5mm to 40mm",
+            bladematerial:"steel",
+            batteryruntime:"50 mins",
+            price:1999,
+            pic:"images/trimmer-16.jpg",
+	},
+    
+	{
+            id:330094, 
+            brand:"nova",
+            color:"blackrose",
+            size:"0.5mm to 20mm",
+            bladematerial:"plastic",
+            batteryruntime:"60 mins",
+            price:999,
+			pic:"images/trimmer-17.jpg",
+       
+	},
+    
+   {
+	        id:091276,
+            brand:"babiajacob",
+            color:"blue",
+            size:"0.5mm to 20mm",
+            bladematerial:"steel",
+            batteryruntime:"50 mins",
+            price:2000,
+			pic:"images/trimmer-18.jpg",
+			
+       
+	},
+    
+	{
+            id:091277,
+            brand:"rdx",
+            color:"yellow-blue",
+            size:"0.5mm to 20mm", 
+            bladematerial:"steel",
+            batteryruntime:"50 mins",
+            price:1000,
+			pic:"images/trimmer-19.jpg",
+       
+	} ,
+    
+    ];
+	
+	let cartitem=[];
+	
+	let qty;
+	
+	
+	
+ //alert("hi")
+ 
+ /* let title=document.getElementById("title_1");
+ title.innerText="ELAMURUHU"; */
+ 
+ //console.log(title);
+	
+	/* let title=document.getElementById("title_1")
+	console.log(title.innerText); */
+	
+	/* let title=document.getElementById("title_1")
+	
+	let getit=document.getElementById("wrap")
+	
+	let para=document.createElement("p");
+	
+	para.innerText="hi all this is elamuruhu";
+	
+	getit.appendChild(para); */
+	/* 
+	let title=document.getElementById("title_1")
+	let getit=document.getElementById("wrap")              //to add class in p
+	let para=document.createElement("p");
+	para.setAttribute("class","para");//to add class attribute and value
+	para.innerText="hi all this is elamuruhu";
+	getit.appendChild(para);
+	
+	 */
+	 
+	 /* <div class="col-item-4">
+	  <div class="card">
+			<div class="card-img">
+				<img src="images/syska-2.jpg" alt="syska">
+			</div>
+			 <div class="card-content">
+				<h2>Syska</h2>
+				<h4>Size| <span>color:black</span></h4>
+				
+				<div>
+					price <span>1000</span>
+				</div>
+			 </div>
+		 </div>
+      </div> */
+	  
+	  
+	 let triElement = document.getElementById("trimmer"); 
+	 let _cartitems = document.getElementById("cartitems");
+	  
+	  trimmer.forEach(function(elm,indx){
+		  
+	  let columnItem = document.createElement("div");
+	  let cardBox = document.createElement("div");
+	  let cardboxImage = document.createElement("div");
+	  let imgEle = document.createElement("img");
+	  
+	  let cardconEle = document.createElement("div");
+	  let headingEle = document.createElement("h2");
+	  let sizeEle = document.createElement("h4");
+	  let colorEle = document.createElement("span");
+	  let priceEle = document.createElement("div");
+	  let endcart = document.createElement("button");
+	  let remove = document.createElement("button");
+	  
+	   //images created
+	  imgEle.src = elm.pic;
+	  cardboxImage.appendChild(imgEle);
+	   cardboxImage.setAttribute("class","card-img");
+	  
+	 //product content
+	 headingEle.innerText = elm.brand;
+	 
+	 
+	 sizeEle.innerText = elm.size;
+	 colorEle.innerText = elm.color;
+	 sizeEle.appendChild(colorEle);
+	 
+	 priceEle.innerText = elm.price;
+	 
+	 endcart.innerText="Add Cart";
+	 /* endcart.setAttribute("class","button"); */
+	 remove.innerText="remove";
+	 
+	 endcart.onclick = search.bind(null,elm.id);
+	 remove.onclick = get.bind(null,elm.id);
+	 
+	  
+	  cardconEle.setAttribute("class","card-content");
+	  cardconEle.append(headingEle,sizeEle,priceEle,endcart,remove);
+	 
+	 cardBox.setAttribute("class","card");
+	 cardBox.append(cardboxImage,cardconEle);
+	 
+	 columnItem.setAttribute("class","col-item-4");
+	 
+	 columnItem.append(cardBox);
+		  
+		
+		triElement.appendChild(columnItem);		
+		  
+	  })
+	  
+	  //add to cart
+	  
+	  function search(id){
+			    let findcart=cartitem.find(function(item){
+				  return item.id===id;
+				});
+				
+			 if(findcart){
+			     let updatecart=cartitem.map(function(item){
+                     if(item.id===id){
+					     item.qty += 1;
+						 item.wholeprice=(item.qty*item.price)
+						 return item;
+					    }
+					 else{
+					    return item;
+					 
+					 }				 
+ 				 });
+			 
+
+				 cartitem=updatecart;
+				 console.log(cartitem)
+				 onshowcart(cartitem);
+			 }
+				
+			 
+			  else{
+			 
+			   let finddata=trimmer.find(function(item){
+		       return item.id===id;
+		       });
+		        finddata.qty=1;
+				finddata.wholeprice=finddata.price;
+		        cartitem.push(finddata);
+	 	        console.log(cartitem);
+				onshowcart(cartitem);
+			 }
+			 }
+	 
+             //remove from cart
+			 
+			 
+             function get(id){
+			     let deleteitem=cartitem.find(function(item)
+				 {
+				    return item.id===id;
+				 
+				 });
+				 if(deleteitem.elm === 1){
+				 
+				     let strikeitem = cartitem.filter(function(item){
+				     
+					      
+					      return item.id !== id;
+					 })
+					    cartitem=strikeitem;
+						console.log(cartitem);
+						onshowcart(cartitem);
+					 }else{
+					        let reduceitem = cartitem.map(function(item){
+							if(item.id === id){
+							item.qty-=1;
+							 item.remainingamount=(item.qty*item.price)
+							
+							return item;
+							}else{
+							     return item;
+								 }
+					})
+					cartitem=reduceitem;
+					console.log(cartitem);
+					onshowcart(cartitem);
+			   }
+			   }
+			   
+			   /*<div class="cart-list">
+			<div class="cart-img">
+				<img src="images/pulsar1.jpg" />
+			</div>
+			<div class="cart-content">
+				<h3>pulsar <span>120</h3>
+				<p><span>2021</sapn> <span>Rs: 91645</span></p>
+				<div class="cart-actions">
+					<button class="in-btn">-</button>
+					<span>1</span>
+					<button class="d-btn">+</button>
+					<button class="delete-btn">Delete</button>
+				</div>
+			</div>
+		</div>*/
+	 
+	function onshowcart(obj){
+		_cartitems.innerHTML = "";
+		let cartBox = obj.map(function(elm, indx){
+			return `<div class="cart-list">
+			<div class="cart-img">
+				<img src="${elm.pic}" />
+			</div>
+			<div class="cart-content">
+				<h3>${elm.brand} <span>${elm.color}</h3>
+				<p><span>${elm.size}</span> <span>Rs: ${elm.price}</span></p>
+				<div class="cart-actions">
+					<button class="in-btn">-</button>
+					<span>${elm.qty}</span>
+					<button class="d-btn">+</button>
+					<button class="delete-btn">Delete</button>
+				</div>
+			</div>
+		</div>`;
+		}).join("")
+		_cartitems.innerHTML = cartBox;
+	}
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
